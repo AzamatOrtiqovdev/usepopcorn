592b1f0716ae14b9378637e617152d7491a763af
AzamatOrtiqovdev <ortiqovazamat890@gmail.com>
2024-08-29 23:04:23 +0500

    use popcorn

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import StarRating from "./StarRating";
+import './index.css';
+import App from './App';
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onRateMovie={setMovieRating} />
+//       <p>This movie was rated {movieRating || ""} stars</p>
+//     </div>
+//   );
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <Test /> */}
+  </React.StrictMode>
+);
